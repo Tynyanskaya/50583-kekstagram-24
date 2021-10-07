5075402353eba9a66a1d493773e7418d7fe4c6bf
Polina <Elemmir89@rambler.ru>
2021-10-08 00:18:09 +0300

    объявила переменную, поставила пробелы

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
-let randomNumber = function(max, min){
-  if (max<= min){
-   console.log(' max не может быть меньше min');
-   return ' ';
+const randomNumber = function (max, min){
+  if (max <= min){
+   console.log('max не может быть меньше min');
+   return '';
   }
-  return Math.round(Math.random()* (max-min +1)+min);
-
+  return Math.round(Math.random() * (max - min + 1) + min);
 };
 console.log(randomNumber(3, 23));
 
 
 let lineLength=function (line, number){
 if (line.length >= number){
-  console.log('Максимальная длина комментария не больше ' + number );
+  console.log('Максимальная длина комментария не больше ' + number);
   return '';
 }
 return line;
 };
-
-phrase = 'Съешь ещё этих мягких французских булок';
+let phrase = 'Съешь ещё этих мягких французских булок';
 console.log(lineLength(phrase, 12));
 
