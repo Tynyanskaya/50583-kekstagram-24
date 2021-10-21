c64746a02f3c5817f070e931e3f623ad20f1b062
Polina <Elemmir89@rambler.ru>
2021-10-21 21:15:52 +0300

    Всё поправила

--- js/main.js
+++ js/main.js
@@ -1,86 +1,78 @@
 const NAMES = ['Артём', 'Катя', 'Вася', 'Марина', 'Алина', 'Света'];
 
-  const MESSAGES = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-    ];
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
 
-    const DESCRIPTIONS = ['№1','№2', '№3', '№4', '№5', '№6', '№7', '№8', '№9', '№10', '№11', '№12', '№13', '№14', '№15,' '№16', '№17', '№18',
-     '№19', '№20', '№21', '№22', '№23', '№24', '№25'];
+const DESCRIPTIONS = ['N1','N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9', 'N10', 'N11', 'N12', 'N13', 'N14', 'N15', 'N16', 'N17', 'N18',
+  'N19', 'N20', 'N21', 'N22', 'N23', 'N24', 'N25'];
 
-     const MAX_PHOTO=25;
-     const MAX_AVATAR=6;
-     const COMMENT_NUM=3;
-     const Likes ={
-         MIN=15,
-         MAX=200
-     };
+const MAX_PHOTO = 25;
+const MAX_AVATAR = 6;
+const COMMENT_NUM = 3;
+const Likes = {
+  MIN: 15,
+  MAX: 200
+};
 
-     let lastId = 0;
-     const getId = function (min, max){
-         lastId += 1;
-         if(lastId<min) lastId=min;
-         if(lastId>max) lastId=max;
-         return lastId;
-     };
+const getRandomNumber = function (first, second) {
+  const min = Math.min(Math.abs(first), Math.abs(second));
+  const max = Math.max(Math.abs(first), Math.abs(second));
+  return (Math.floor(Math.random() * (max - min + 1) + min));
+  };
 
+  const getRandomElement = function (array) {
+    const element = array[getRandomNumber(1, array.length)];
+    return element;
+    }
 
-     const getRandomNumber = function (first, second) {
-         const min = Math.min(Math.abs(first), Math.abs(second));
-         const max = Math.max(Math.abs(first), Math.abs(second));
-         return (Math.floor(Math.random() * (max - min + 1) + min));
-       };
+const getPhotoUrl = function(id) {
+  return 'photos/' + id + '.jpg';
+};
 
-     const getPhotoUrl = function(id){
-         return 'photos/' + id + '.jpg';
-     };
+const getAvatar = function(i) {
+  return 'img/avatar-' + getRandomNumber(1, i) + '.svg';
+};
 
-     const getAvatar = function(i){
-         return 'img/avatar-' + getRandomNumber(1, i) + '.svg';
-     };
+const getPhotos = function (count) {
+  const photos = [];
+    for (let i = 0; i < count; i++) {
+        const id = i + 1;
+        const photo = getPhoto(id);
+        photos[i] = photo;
+    }
+  return photos;
+};
 
-     const randomName = Math.round(Math.random() * NAMES.length);
-     const randomDescription = Math.round(Math.random() * DESCRIPTIONS.length);
-     const randomMessage = Math.round(Math.random() * MESSAGES.length);
+const getPhoto = function (id) {
+  return {
+    id: id,
+    url: getPhotoUrl(id),
+    description: getRandomElement(DESCRIPTIONS),
+    likes: getRandomNumber(Likes.MIN, Likes.MAX),
+    comments: getComments(COMMENT_NUM),
+  }
+};
 
-     const getPhotos = function (MAX_PHOTO){
-         const photos=[];
-         for(let i=0; i<MAX_PHOTO; i++){
-             const id = i+1;
-             const photo = getPhoto(id);
-             photos[i]=photo;
-         }
-         return photos;
-     };
+const getComments = function (count) {
+ const comments = [];
+    for (let i = 0; i < count; i++){
+      const id = i + 1;
+      comments[i] = getRandomComment(id);
+    }
+  return comments;
+};
 
-     const getPhoto = function (id){
-         return{
-               id: id,
-               url: getPhotoUrl(id),
-               description: DESCRIPTIONS[randomDescription],
-               likes: getRandomNumber(Likes.MIN, Likes.MAX),
-               comments: getRandomComments(COMMENT_NUM),
-         }
-     };
-
-     const getComments = function (cn){
-         const comments=[];
-         for(let i=0; i<cn; i++){
-             const idc = i+1;
-             comments[i] = getComment();
-         }
-         return comments;
-     };
-
-     const getRandomComment = function(id){
-     return {
-         id: Math.round(Math.random()*100),
-         avatar: getAvatar(MAX_AVATAR),
-         message: MESSAGES[randomMessage],
-         name: NAMES[randomName],
-      }
-     };
+const getRandomComment = function(id) {
+ return {
+    id: Math.round(Math.random()*100),
+    avatar: getAvatar(MAX_AVATAR),
+    message: getRandomElement(MESSAGES),
+    name: getRandomElement(NAMES),
+ }
+};
