acad2c67559b1441cb642e5a95f8d0c2b63b5da3
Polina <Elemmir89@rambler.ru>
2021-10-23 16:50:49 +0300

    Теперь всё дб исправлено

--- js/main.js
+++ js/main.js
@@ -6,47 +6,56 @@ const MESSAGES = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
-const DESCRIPTIONS = ['N1','N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9', 'N10', 'N11', 'N12', 'N13', 'N14', 'N15', 'N16', 'N17', 'N18',
-  'N19', 'N20', 'N21', 'N22', 'N23', 'N24', 'N25'];
+const DESCRIPTIONS = ['№1','№2', '№3', '№4', '№5', '№6', '№7', '№8', '№9', '№10', '№11', '№12', '№13', '№14', '№15', '№16', '№17', '№18',
+  '№19', '№20', '№21', '№22', '№23', '№24', '№25'];
 
+// eslint-disable-next-line no-unused-vars
 const MAX_PHOTO = 25;
 const MAX_AVATAR = 6;
 const COMMENT_NUM = 3;
 const Likes = {
   MIN: 15,
-  MAX: 200
+  MAX: 200,
 };
 
 const getRandomNumber = function (first, second) {
   const min = Math.min(Math.abs(first), Math.abs(second));
   const max = Math.max(Math.abs(first), Math.abs(second));
   return (Math.floor(Math.random() * (max - min + 1) + min));
-  };
+};
 
-  const getRandomElement = function (array) {
-    const element = array[getRandomNumber(1, array.length)];
-    return element;
-    }
+const getRandomElement = function (array) {
+  const element = array[getRandomNumber(1, array.length)];
+  return element;
+};
 
 const getPhotoUrl = function(id) {
-  return 'photos/' + id + '.jpg';
+  return `photos/${ id }.jpg`;
 };
 
 const getAvatar = function(i) {
-  return 'img/avatar-' + getRandomNumber(1, i) + '.svg';
+  return `img/avatar-${  getRandomNumber(1, i)  }.svg`;
 };
 
-const getPhotos = function (count) {
-  const photos = [];
-    for (let i = 0; i < count; i++) {
-        const id = i + 1;
-        const photo = getPhoto(id);
-        photos[i] = photo;
-    }
-  return photos;
+const getRandomComment = function() {
+  return {
+    id: Math.round(Math.random()*100),
+    avatar: getAvatar(MAX_AVATAR),
+    message: getRandomElement(MESSAGES),
+    name: getRandomElement(NAMES),
+  };
+};
+
+const getComments = function (count) {
+  const comments = [];
+  for (let i = 0; i < count; i++){
+    const id = i + 1;
+    comments[i] = getRandomComment(id);
+  }
+  return comments;
 };
 
 const getPhoto = function (id) {
@@ -56,23 +65,17 @@ const getPhoto = function (id) {
     description: getRandomElement(DESCRIPTIONS),
     likes: getRandomNumber(Likes.MIN, Likes.MAX),
     comments: getComments(COMMENT_NUM),
+  };
+};
+
+const getPhotos = function (count) {
+  const photos = [];
+  for (let i = 0; i < count; i++) {
+    const id = i + 1;
+    const photo = getPhoto(id);
+    photos[i] = photo;
   }
+  return photos;
 };
+getPhotos();
 
-const getComments = function (count) {
- const comments = [];
-    for (let i = 0; i < count; i++){
-      const id = i + 1;
-      comments[i] = getRandomComment(id);
-    }
-  return comments;
-};
-
-const getRandomComment = function(id) {
- return {
-    id: Math.round(Math.random()*100),
-    avatar: getAvatar(MAX_AVATAR),
-    message: getRandomElement(MESSAGES),
-    name: getRandomElement(NAMES),
- }
-};
